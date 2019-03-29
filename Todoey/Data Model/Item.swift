6f4809e343186856b55d9438ad565fe340d5fc88
pawel <pawel.wisniewski@s-labs.io>
2019-03-29 09:24:51 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Paweł on 29.03.2019.
+//  Copyright © 2019 S Labs. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
